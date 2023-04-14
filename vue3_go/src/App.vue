db0b27dca18cab3c66e1e3ea62875ee520509a81
libingle <2523890936@qq.com>
2023-04-14 21:59:07 +0800

    vue3

---
+++ vue3_go/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <h1>我是App。。组件</h1>
+  <h2>我的名字是：{{ person.name }}</h2>
+  <h2>我的年龄是：{{ person.age }}</h2>
+  <button @click="gai">修改</button>
+</template>
+
+<script>
+import {reactive, watch} from "vue";
+
+export default {
+  name: 'App',
+  components: {},
+  setup() {
+    //数据
+    let person = reactive({
+      name: '张三',
+      age: 23
+    })
+    function gai(){
+      return person.age=person.age*10
+
+    }
+    watch(person, (newValue, oldValue) => {
+      console.log('person变化了',newValue,oldValue)
+    })
+    //方法
+    return {
+      person,
+      gai
+
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
